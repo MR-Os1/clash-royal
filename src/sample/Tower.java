3ee22d3c5aa5cb27f9b8ab2a8af4a528bd30bf0e
Mohammad Reza Ostadi <mr.os7798@gmail.com>
2021-02-02 19:03:06 +0330

    Create abstract base classes

--- src/sample/Tower.java
+++ src/sample/Tower.java
@@ -1,4 +1,54 @@
 package sample;
 
-public class Tower {
+import javafx.scene.shape.Shape;
+
+public abstract class Tower {
+
+
+    private Shape shape;
+
+    private Integer health;
+
+    private Integer damage;
+
+    private Integer range;
+
+    public Tower(Shape shape,int health,int damage,int range) {
+        this.shape = shape;
+        this.damage = damage;
+        this.range = range;
+        this.health = health;
+    }
+
+    public Integer getHealth() {
+        return health;
+    }
+
+    public void setHealth(Integer health) {
+        this.health = health;
+    }
+
+    public Integer getDamage() {
+        return damage;
+    }
+
+    public void setDamage(Integer damage) {
+        this.damage = damage;
+    }
+
+    public Integer getRange() {
+        return range;
+    }
+
+    public void setRange(Integer range) {
+        this.range = range;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
 }
--- src/sample/Unit.java
+++ src/sample/Unit.java
@@ -1,4 +1,83 @@
 package sample;
 
-public class Unit {
+import javafx.scene.shape.Shape;
+import javafx.scene.text.Text;
+
+public abstract class Unit {
+
+    private int energy;
+
+    private int life;
+
+    private int speed;
+
+    private int range;
+
+    private int damage;
+
+    private Shape shape;
+
+    private Text text;
+
+    public Unit(int energy, int life, int speed, int range, int damage, Shape shape,Text text) {
+        this.energy = energy;
+        this.life = life;
+        this.speed = speed;
+        this.range = range;
+        this.damage = damage;
+        this.shape = shape;
+        this.text= text ;
+    }
+
+    public Shape[] getShapes(){
+        return new Shape[]{getShape(), text};
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
 }
